299e628ffb63dd800c2c663bc4cd07e28b34d191
Jesse McNichol <5956604+jcmcnch@users.noreply.github.com>
2022-05-10 13:53:17 -0700

    Add updated files / scripts

---
+++ scripts/01-get-text-recursively.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -i
+conda activate beautifulsoup
+
+#specify output file
+outfile=results/01-scraped-text.tsv
+rm $outfile 2> /dev/null
+
+#remove any stray webpages
+rm index.html 2> /dev/null
+
+#recurse through results
+for item in `cut -f2 results/00-links-BODC.tsv`; do
+
+	BODClink=`echo $item | cut -f2 -d" "`
+	wget $BODClink 2> /dev/null
+
+	#use beautifulsoup to parse text from HTML then grep / tr to slice & clean up
+	scrapedText=`./scripts/beautifulsoup-gettext.py | grep -z -o -P '(?<=Acquisition description)(?s).*(?=References Cited)' | tr '\n' ' ' | tr -d '\0'`
+
+	#print to tsv
+	printf "$item\t$scrapedText\n" > $outfile
+
+	#remove any stray webpages
+	rm index.html 2> /dev/null
+done
